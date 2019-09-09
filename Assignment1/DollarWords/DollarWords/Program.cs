f127657e5d6efe9844f7e3f767e9ebba489c4a12
Jennifer Stone <jennifervstone@gmail.com>
2019-09-09 10:45:56 -0400

    Added ability to calculate the longest and shortest dollar words and output information to console. Can now output dollar words to file

--- Assignment1/DollarWords/DollarWords/Program.cs
+++ Assignment1/DollarWords/DollarWords/Program.cs
@@ -9,17 +9,18 @@ namespace DollarWords
     {
         const int CharacterOffset = (int)'A' - 1;
         const string InputFilePath = @"C:\Users\jenni\Desktop\Fall 2019\COS 470\Cos470\Assignment1\DollarWords\words.txt";
+        const string OutputFilePath = @"C:\Users\jenni\Desktop\Fall 2019\COS 470\Cos470\Assignment1\DollarWords\DollarWords.txt";
         static void Main(string[] args)
         {
-            StreamWriter outputFile = new StreamWriter(@"C:\Users\jenni\Desktop\Fall 2019\COS 470\Cos470\Assignment1\DollarWords\DollarWords.txt");
 
-            var words = readWords(InputFilePath);
-           
-            outputFile.Dispose();
+            var words = ReadWords(InputFilePath);
+            var dollarWords = GetDollarWords(words);
+            OutputToFile(OutputFilePath, dollarWords);
+            OutputAggregateDataToConsole(words, dollarWords);
             Console.Read();
-        }   
-        
-        public static List<string> readWords(String path)
+        }
+
+        public static List<string> ReadWords(String path)
         {
             var words = new List<string>();
             using (StreamReader stream = new StreamReader(path))
@@ -32,13 +33,28 @@ namespace DollarWords
             return words;
         }
 
-        public static List<string> getDollarWords(List<string> words) 
+        public static List<string> GetDollarWords(List<string> words)
         {
             return words.Where(w => StringToIntegerValue(w) == 100).ToList();
         }
 
+        public static string GetMostExpensiveWord(List<string> words)
+        {
+            return words.OrderByDescending(w => StringToIntegerValue(w)).First();
+        }
+
+        public static string GetLongestWord(List<String> words)
+        {
+            return words.OrderByDescending(w => w.Length).First();
+        }
+
+        public static string GetShortestWord(List<String> words)
+        {
+            return words.OrderBy(w => w.Length).First();
+        }
+
         /// <summary>
-        /// Returns an interger value of a character where letter a-z correspond to 0-26 and all other characters to 0.
+        /// Returns an integer value of a character where letter a-z correspond to 1-26 and all other characters to 0.
         /// </summary>
         /// <param name="character"></param>
         /// <returns></returns>
@@ -55,6 +71,30 @@ namespace DollarWords
                 wordValue += charVal;
             }
             return wordValue;
+        }
+
+        /// <summary>
+        /// Output aggregate information
+        /// 
+        /// </summary>
+        /// <param name="words"></param>
+        /// <param name="dollarWords"></param>
+        public static void OutputAggregateDataToConsole(List<string> words, List<string> dollarWords)
+        {
+            Console.WriteLine("Portion of words that are dollar words: {0:P4}", (double)dollarWords.Count / words.Count);
+            Console.WriteLine("Longest dollar word: {0}", GetLongestWord(dollarWords));
+            Console.WriteLine("Shortest dollar word: {0}", GetShortestWord(dollarWords));
+            Console.WriteLine("Most Expensive word: {0}", GetMostExpensiveWord( words));
+        }
+
+        public static void OutputToFile(string outputPath, List<string> words)
+        {
+            using (StreamWriter outputFile = new StreamWriter(outputPath))
+            {
+                foreach(string word in words){
+                    outputFile.WriteLine(word);
+                }
+            }
         }
     }
 
