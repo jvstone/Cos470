9540c45ab9f8312d150eb1113f0bc485d86c9c83
Jennifer Stone <jennifervstone@gmail.com>
2019-09-09 10:17:08 -0400

    Refactored and added ability to get the most expensive word

--- Assignment1/DollarWords/DollarWords/Program.cs
+++ Assignment1/DollarWords/DollarWords/Program.cs
@@ -1,52 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+
 namespace DollarWords
 {
     class Program
     {
         const int CharacterOffset = (int)'A' - 1;
+        const string InputFilePath = @"C:\Users\jenni\Desktop\Fall 2019\COS 470\Cos470\Assignment1\DollarWords\words.txt";
         static void Main(string[] args)
         {
-            StreamReader file = new StreamReader(@"C:\Users\jenni\Desktop\Fall 2019\COS 470\Cos470\Assignment1\DollarWords\words.txt");
             StreamWriter outputFile = new StreamWriter(@"C:\Users\jenni\Desktop\Fall 2019\COS 470\Cos470\Assignment1\DollarWords\DollarWords.txt");
-            int dollarWordCount = 0;
-            while (!file.EndOfStream)
-            {
-                int wordValue = 0;
-                String word = file.ReadLine();
-                foreach(char c in word)
-                {
-                    wordValue += LetterToIntegerValue(c);
-                }
-                if(wordValue == 100)
-                {
-                    dollarWordCount++;
-                    outputFile.WriteLine(word);
-                }
-             
-            }
-            Console.WriteLine(dollarWordCount + " dollar words");
-            file.Dispose();
-            outputFile.Close();
+
+            var words = readWords(InputFilePath);
+           
+            outputFile.Dispose();
             Console.Read();
         }   
         
+        public static List<string> readWords(String path)
+        {
+            var words = new List<string>();
+            using (StreamReader stream = new StreamReader(path))
+            {
+                while (!stream.EndOfStream)
+                {
+                    words.Add(stream.ReadLine());
+                }
+            }
+            return words;
+        }
+
+        public static List<string> getDollarWords(List<string> words) 
+        {
+            return words.Where(w => StringToIntegerValue(w) == 100).ToList();
+        }
 
         /// <summary>
         /// Returns an interger value of a character where letter a-z correspond to 0-26 and all other characters to 0.
         /// </summary>
         /// <param name="character"></param>
         /// <returns></returns>
-        public static int LetterToIntegerValue(char character)
+        public static int StringToIntegerValue(String word)
         {
-            int charVal = 0;
-            if (char.IsLetter(character))
+            int wordValue = 0;
+            foreach (char character in word)
             {
-               character =  Char.ToUpper(character);
-                charVal = (int)character - CharacterOffset;
+                int charVal = 0;
+                if (char.IsLetter(character))
+                {
+                    charVal = (int)Char.ToUpper(character) - CharacterOffset;
+                }
+                wordValue += charVal;
             }
-            return charVal;
+            return wordValue;
         }
     }
+
 }
 
