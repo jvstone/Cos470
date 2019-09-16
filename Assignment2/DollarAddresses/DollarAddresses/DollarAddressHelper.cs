ff25881a095cda23e8015d25845bf390ab64ea6a
Jennifer Stone <jennifervstone@gmail.com>
2019-09-15 23:18:39 -0400

    Started Assignment2, added config file and Dollar address helper class

---
+++ Assignment2/DollarAddresses/DollarAddresses/DollarAddressHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DollarAddresses
+{
+    class DollarAddressHelper
+    {
+        int characterOffset;
+
+
+        public DollarAddressHelper(int characterOffset)
+        {
+            this.characterOffset = characterOffset;
+        }
+
+        /// <summary>
+        /// Return a list of words whose value sums to 100 
+        /// </summary>
+        /// <param name="words"></param>
+        /// <returns></returns>
+        public List<string> GetDollarWords(List<string> words)
+        {
+            return words.Where(w => StringToIntegerValue(w) == 100).ToList();
+        }
+
+        /// <summary>
+        /// Returns an integer value of a character where letter a-z correspond to 1-26 and all other characters to 0.
+        /// </summary>
+        /// <param name="character"></param>
+        /// <returns></returns>
+        public  int StringToIntegerValue(String word)
+        {
+            int wordValue = 0;
+            foreach (char character in word)
+            {
+                int charVal = 0;
+                if (char.IsLetter(character))
+                {
+                    charVal = (int)Char.ToUpper(character) - characterOffset;
+                }
+                wordValue += charVal;
+            }
+            return wordValue;
+        }
+    }
+}
---
+++ Assignment2/DollarAddresses/DollarAddresses/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DollarAddresses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                              .AddJsonFile("appsettings.json", false, true)
+                              .Build();
+            var offset = int.Parse( config["CharOffset"]);
+            var dollarAddresses = new DollarAddressHelper(offset);
+        }
+    }
+}
