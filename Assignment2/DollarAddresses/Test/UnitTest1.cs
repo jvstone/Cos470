84dec4850fc4979ca6d725ddfb4b4108a063b6dc
Jennifer Stone <jennifervstone@gmail.com>
2019-09-19 15:57:45 -0400

    Started adding unit tests

---
+++ Assignment2/DollarAddresses/Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase("a", ExpectedResult = 1)]
+        [TestCase("", ExpectedResult = 0)]
+        [TestCase("abc", ExpectedResult = 6)]
+        [TestCase("abc123", ExpectedResult = 6)]
+        [TestCase("abc123xyz", ExpectedResult = 81)]
+        [TestCase("a b c d", ExpectedResult = 10)]
+        public int TestDollarAddressHelperStringToIntegerValue(string input)
+        {
+            var helper = new DollarAddresses.DollarAddressHelper(64);
+            return helper.StringToIntegerValue(input);
+        }
+    }
+}
---
+++ Assignment2/DollarAddresses/Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase("a", ExpectedResult = 1)]
+        [TestCase("", ExpectedResult = 0)]
+        public void Test1()
+        {
+            var helper = new Doll
+            return 
+                
+        }
+    }
+}
